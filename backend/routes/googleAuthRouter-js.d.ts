b90c0aff5e45d59becb3a2ffbeb5f38102d4e0d7
Marijan Kolaj <marijankolaj@gmail.com>
2025-12-13 13:27:11 +0100

    removed the breaking functions

--- backend/routes/googleAuthRouter-js.d.ts
+++
@@ -1,3 +0,0 @@
-// TypeScript declaration for googleAuthRouter.js
-// This allows TypeScript to treat the module as 'any' and suppresses import warnings.
-declare module '../../routes/googleAuthRouter.js';
